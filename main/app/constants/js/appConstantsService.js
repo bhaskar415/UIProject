ead790007de73bcaa312ca98f8d15cb0f3fad41b
bhaskar415 <bhaskar_415@yahoo.com>
2015-03-10 15:12:17 -0400

    Update appConstantsService.js

--- main/app/constants/js/appConstantsService.js
+++ main/app/constants/js/appConstantsService.js
@@ -1,86 +1,28 @@
-var myApp = angular.module('app-constants', []);
-
-myApp.config(function($httpProvider) {
-    //Enable cross domain calls
-    $httpProvider.defaults.useXDomain = true;
-
-    //Remove the header containing XMLHttpRequest used to identify ajax call 
-    //that would prevent CORS from working
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-});
-
+var myApp = angular.module('App-Constants-Service', []);
 
 myApp.service(
-			"httpCURDOperations",
-			function($http, $q) {				
-		
-		this.Get = function( url ) {
-			var request = $q.defer();
-			$http({
-					method: "get",
-					url: url,
-					params: {
-						action: "get"
-					}
-				}).success(function(data){
-				request.resolve(data); // retriving particular element dynamically by passing the element name
-				}).error(function(){
-				request.reject('There is an error')
-				})
+			"ConstructUrls",
+			function() {				
 
-				return request.promise;					
+			var baseUrl = 'http://10.215.192.72:8080';
+			//var baseUrl = 'http://192.168.1.18:8080';
+			//var baseUrl = 'http://localhost:8080';
+			var _calledUrl = '';
+			var _finalUrl = '';
+
+			this.makeUrl = function(calledUrl) { 
+               _calledUrl = calledUrl;
+			   _finalUrl = baseUrl + calledUrl;
+					return _finalUrl;		
+			
 			};
-		
-		this.Add = function( url, data ) { 
-  
-              var request = $q.defer();
-				$http({
-						method: "post",
-						url: url,
-						params: {
-							action: "add"
-						},
-						data: data
-					}).success(function(data){
-					request.resolve(data);
-					}).error(function(){
-					request.reject('There is an error')
-					})
-
-					return request.promise;		
+			
+			this.baseUrl = function() { 
+			   _finalUrl = baseUrl;
+					return _finalUrl;		
 			
 			};
 		
-		this.Delete = function( url ) {			  
-
-      		var request = $q.defer();				
-				var request = $http({
-						method: "delete",
-						url: url,
-						params: {
-							action: "delete"
-						}
-					}).error(function(){
-					request.reject('There is an error')
-					}) 
-					
-			};
-
-		this.Update = function( url, data ) { 
-				
-				var request = $q.defer();				
-				var request = $http({
-						method: "patch",
-						url: url,
-						params: {
-							action: "update"
-						},
-						data: data
-					}).error(function(){
-					request.reject('There is an error')
-					}) 
-					
-			};			
 			
 			}
-		)
+);
